40f5df5ebf04c209aca7d4fd42ef15a4e12f38fb
mdayyan011 <mdayyanfahim@gmail.com>
2024-03-10 14:45:15 +0530

    formHandling|10032024

---
+++ product-gallery-app/src/Create.jsx
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+
+const Create = () => {
+  let [isClicked, setIsClicked] = useState(true);
+  let [passwordType, setPasswordType] = useState('password');
+
+  let [price, setPrice] = useState(0);
+  let [name, setName] = useState('');
+  let [desc, setdesc] = useState('');
+  let [imgSrc, setimgSrc] = useState('');
+  let [category, setcategory] = useState('');
+
+  const changeVisiblity = () => {
+    setIsClicked(!isClicked);
+  };
+  const changePasswordType = () => {
+    if (passwordType === 'password') {
+      setPasswordType('text');
+    } else if (passwordType === 'text') {
+      setPasswordType('password');
+    }
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(name);
+    console.log(price);
+    console.log(desc);
+    console.log(imgSrc);
+    console.log(category);
+    console.log('Hey there I got submitted');
+  };
+  return (
+    <>
+      <div className='create-container'>
+        <h1 className='create-heading'>Create</h1>
+        <button className='create-btn' onClick={changeVisiblity}>
+          +
+        </button>
+      </div>
+      {isClicked && (
+        <div className='form-container'>
+          <form onSubmit={handleSubmit}>
+            <div className='input-container'>
+              <label className='label-txt'>Product Name</label>
+              <input
+                type='text'
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+              />
+            </div>
+            <div className='input-container'>
+              <label className='label-txt'>Product Price</label>
+              <input
+                type='number'
+                value={price}
+                onChange={(event) => setPrice(event.target.value)}
+              />
+            </div>
+            <div className='input-container'>
+              <label className='label-txt'>Product Description</label>
+              <input
+                type='text'
+                value={desc}
+                onChange={(event) => setdesc(event.target.value)}
+              />
+            </div>
+
+            <div className='input-container'>
+              <label className='label-txt'>Product Image</label>
+              <input
+                type='text'
+                value={imgSrc}
+                onChange={(event) => setimgSrc(event.target.value)}
+              />
+            </div>
+            <div className='input-container'>
+              <label className='label-txt'>Product Category</label>
+              <select onChange={(event) => setcategory(event.target.value)}>
+                <option value='electronic'>Electronic</option>
+                <option value='abc'>abc</option>
+                <option value='exs'>exs</option>
+                <option value='pqr'>pqr</option>
+              </select>
+            </div>
+
+            <button type='submit' className='create-btn'>
+              Submit
+            </button>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Create;
--- product-gallery-app/src/Gallery.jsx
+++ product-gallery-app/src/Gallery.jsx
@@ -1,34 +1,46 @@
 import { useState, useEffect } from 'react';
+import Create from './Create';
 
 const Gallery = () => {
   let [isLoaded, setIsLoaded] = useState(false);
-  // let productData = [];
   let [productData, setProductData] = useState(null);
 
   useEffect(() => {
     setInterval(() => setIsLoaded(true), 1000);
 
     // Calling API
-    fetch('https://fakestoreapi.com/products?limit=5')
+    fetch('https://fakestoreapi.com/products')
       .then((data) => {
         console.log(data);
         return data.json();
       })
-        .then((jsonData) => setProductData(jsonData))
-        .catch(error => console.log(error));
+      .then((jsonData) => setProductData(jsonData))
+      .catch((error) => console.log(error));
   }, []);
 
   return (
     <div>
-      {!isLoaded ? <h1> Loading....</h1> : <h1>Welcome to my Home Page</h1>}
-      {productData &&
-        productData.map((product) => (
-          <div className='main' key={product.id}>
-            <h1>{product.title}</h1>
-            <img src={product.image} className='image-container' />
-            <h2>${product.price}</h2>
-          </div>
-        ))}
+      {!isLoaded ? (
+        <h1> Loading....</h1>
+      ) : (
+        productData && (
+          <>
+            <Create />
+            <h1 style={{ color: '#000', fontWeight: 'bold', fontSize: '35px' }}>
+              All Products
+            </h1>
+            <div className='card-container'>
+              {productData.map((product) => (
+                <div className='card' key={product.id}>
+                  <h1>{product.title}</h1>
+                  <img src={product.image} className='image-container' />
+                  <h2>${product.price}</h2>
+                </div>
+              ))}
+            </div>
+          </>
+        )
+      )}
     </div>
   );
 };
