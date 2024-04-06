02e47e3dd9634e591518ddb73f05b8fad51eaad7
Md Ayyan Fahim <65223164+mdayyan011@users.noreply.github.com>
2024-04-06 17:22:51 +0530

    Delete day04-app directory

--- day04-app/src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Card from './Card';
-import Gallery from './Gallery';
-
-import './index.css';
-ReactDOM.render(<Gallery />, document.getElementById('root'));
